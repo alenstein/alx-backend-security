0a7d1a0ccbfcd1ae761c29d382c136fde77a5dd6
Allen Sibanda <sibandallen@gmail.com>
2025-11-09 00:29:12 +0200

    fix: anormally detection by path access

--- ip_tracking/ip_tracking/tasks.py
+++ ip_tracking/ip_tracking/tasks.py
@@ -6,7 +6,7 @@ from .models import RequestLog, SuspiciousIP
 
 
 @shared_task
-def detect_suspicious_ips():
+def detect_suspicious_ips(path__startswith=None):
     """
     A Celery task to detect and flag suspicious IP addresses based on request patterns.
     Runs hourly to check for:
@@ -16,15 +16,8 @@ def detect_suspicious_ips():
     one_hour_ago = timezone.now() - timedelta(hours=1)
 
     # 1. Find IPs with high request volume
-    high_volume_ips = RequestLog.objects.filter(
-        timestamp__gte=one_hour_ago
-    ).values(
-        'ip_address'
-    ).annotate(
-        request_count=Count('id')
-    ).filter(
-        request_count__gt=100
-    )
+    high_volume_ips = RequestLog.objects.filter(timestamp__gte=one_hour_ago).values('ip_address').annotate(\
+        request_count=Count('id')).filter(request_count__gt=100)
 
     for item in high_volume_ips:
         reason = f"High request volume: {item['request_count']} requests in the last hour."
@@ -34,14 +27,11 @@ def detect_suspicious_ips():
         )
 
     # 2. Find IPs accessing sensitive paths
-    sensitive_paths_ips = RequestLog.objects.filter(
-        timestamp__gte=one_hour_ago,
-    ).filter(
-        Q(path__startswith='/admin/') | Q(path__startswith='/login/')
-    ).values_list('ip_address', flat=True).distinct()
+    sensitive_paths_ips = RequestLog.objects.filter(timestamp__gte=one_hour_ago).filter(Q(path__startswith='/admin/')\
+        | Q(path__startswith='/login/')).values_list('ip_address', flat=True).distinct()
 
     for ip in sensitive_paths_ips:
-        reason = "Accessed a sensitive path (e.g., /admin/, /login/)."
+        reason = "Accessed a sensitive path ( /admin/, /login/)."
         SuspiciousIP.objects.update_or_create(
             ip_address=ip,
             defaults={'reason': reason}
